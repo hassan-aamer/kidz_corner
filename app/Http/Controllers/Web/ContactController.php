6d6599029f495548ee1c0c2484dc57011d0c7f71
hassan-aamer <hassanaamer048@gmail.com>
2025-08-21 03:07:50 +0300

    feat: Implement contact page with dynamic categories and contact form

--- app/Http/Controllers/Web/ContactController.php
+++ app/Http/Controllers/Web/ContactController.php
@@ -4,14 +4,24 @@ namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
 use App\Services\Contact\ContactService;
+use App\Services\Categories\CategoryService;
 use App\Http\Requests\Contact\ContactRequest;
 
 class ContactController extends Controller
 {
     protected ContactService $service;
-    public function __construct(ContactService $service)
+    protected CategoryService $categoryService;
+    public function __construct(ContactService $service, CategoryService $categoryService)
     {
         $this->service = $service;
+        $this->categoryService = $categoryService;
+    }
+    public function index()
+    {
+        $result = [
+            'categories_search' => $this->categoryService->index()->where('active', 1)->take(10),
+        ];
+        return view('web.pages.contact', compact('result'));
     }
     public function store(ContactRequest $request)
     {
---
+++ resources/views/web/pages/contact.blade.php
@@ -0,0 +1,110 @@
+@extends('web.layouts.app')
+@section('title', __('attributes.contacts'))
+@section('content')
+
+    <!-- Navbar Start -->
+    <div class="container-fluid">
+        <div class="row border-top px-xl-5">
+            <div class="col-lg-3 d-none d-lg-block">
+                <a class="btn shadow-none d-flex align-items-center justify-content-between bg-primary text-white w-100"
+                    data-toggle="collapse" href="#navbar-vertical" style="height: 65px; margin-top: -1px; padding: 0 30px;">
+                    <h6 class="m-0">Categories</h6>
+                    <i class="fa fa-angle-down text-dark"></i>
+                </a>
+                <nav class="collapse position-absolute navbar navbar-vertical navbar-light align-items-start p-0 border border-top-0 border-bottom-0 bg-light"
+                    id="navbar-vertical" style="width: calc(100% - 30px); z-index: 1;">
+                    <div class="navbar-nav w-100 overflow-hidden" style="height: 410px">
+                        @foreach ($result['categories_search']->sortBy('position') as $categories_search)
+                            <a href="" class="nav-item nav-link">{{ $categories_search->title ?? '' }}</a>
+                        @endforeach
+                    </div>
+                </nav>
+            </div>
+            <div class="col-lg-9">
+                @include('web.layouts.nav')
+            </div>
+        </div>
+    </div>
+    <!-- Navbar End -->
+
+
+    <!-- Page Header Start -->
+    <div class="container-fluid bg-secondary mb-5">
+        <div class="d-flex flex-column align-items-center justify-content-center" style="min-height: 300px">
+            <h1 class="font-weight-semi-bold text-uppercase mb-3">Contact</h1>
+            <div class="d-inline-flex">
+                <p class="m-0"><a href="">Home</a></p>
+                <p class="m-0 px-2">-</p>
+                <p class="m-0">Contact</p>
+            </div>
+        </div>
+    </div>
+    <!-- Page Header End -->
+
+
+    <!-- Contact Start -->
+    <div class="container-fluid pt-5">
+        <div class="text-center mb-4">
+            <h2 class="section-title px-5"><span class="px-2">Contact For Any Queries</span></h2>
+        </div>
+        <div class="row px-xl-5">
+            <div class="col-lg-7 mb-5">
+                <div class="contact-form">
+                    <div id="success"></div>
+                    <form action="{{ route('contact.store') }}" method="post">
+                        @csrf
+                        <div class="control-group">
+                            <input type="text" class="form-control  @error('name') is-invalid @enderror" id="name"
+                                name="name" placeholder="Your Name" required="required"
+                                data-validation-required-message="Please enter your name" />
+                            <p class="help-block text-danger"></p>
+                            @error('name')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="control-group">
+                            <input type="email" class="form-control  @error('email') is-invalid @enderror" id="email"
+                                name="email" placeholder="Your Email" required="required"
+                                data-validation-required-message="Please enter your email" />
+                            <p class="help-block text-danger"></p>
+                            @error('email')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="control-group">
+                            <input type="text" class="form-control  @error('phone') is-invalid @enderror"
+                                id="phone" name="phone" placeholder="phone" required="required"
+                                data-validation-required-message="Please enter a phone number" />
+                            <p class="help-block text-danger"></p>
+                            @error('phone')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="control-group">
+                            <textarea class="form-control  @error('message') is-invalid @enderror" rows="6" id="message" name="message"
+                                placeholder="Message" required="required" data-validation-required-message="Please enter your message"></textarea>
+                            <p class="help-block text-danger"></p>
+                            @error('message')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div>
+                            <button class="btn btn-primary py-2 px-4" type="submit" id="sendMessageButton">Send
+                                Message</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="col-lg-5 mb-5">
+                <div class="d-flex flex-column mb-3">
+                    <h5 class="font-weight-semi-bold mb-3">Store 1</h5>
+                    <p class="mb-2"><i class="fa fa-map-marker-alt text-primary mr-3"></i>{{ setting('address') }}</p>
+                    <p class="mb-2"><i class="fa fa-envelope text-primary mr-3"></i>{{ setting('email') }}</p>
+                    <p class="mb-2"><i class="fa fa-phone-alt text-primary mr-3"></i>{{ setting('phone') }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Contact End -->
+
+@endsection
