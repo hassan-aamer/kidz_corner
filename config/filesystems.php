68d715bdd0449483d37d8981812edca01963d103
hassan-aamer <hassanaamer048@gmail.com>
2025-08-25 20:12:21 +0300

    update

--- config/filesystems.php
+++ config/filesystems.php
@@ -62,11 +62,10 @@ return [
         'media' => [
             'driver' => 'local',
             'root'   => public_path('media'),
-            'url'    => env('APP_URL') . '/public/media',
+            'url'    => env('APP_URL') . '/media',
             'visibility' => 'public',
             'throw' => false,
         ],
-
     ],
 
     /*
--- resources/views/web/components/category-item.blade.php
+++ resources/views/web/components/category-item.blade.php
@@ -1,7 +1,7 @@
                     <div class="col-lg-4 col-md-6 pb-1">
                         <div class="cat-item d-flex flex-column border mb-4" style="padding: 30px;">
                             <p class="text-right">{{ $categories->products->count() ?? '' }} Products</p>
-                            <a href="" class="cat-img position-relative overflow-hidden mb-3">
+                            <a href="{{ route('products.category', $categories->id) }}" class="cat-img position-relative overflow-hidden mb-3">
                                 <img class="img-fluid"
                                     src="{{ App\Helpers\Image::getMediaUrl($categories, 'repositories') }}"
                                     alt="{{ $categories->title ?? '' }}"  loading="lazy">
