7421a5e45399a722c8b0c5c52baa61d228eca538
hassan-aamer <hassanaamer048@gmail.com>
2025-08-26 00:15:27 +0300

    Update

--- resources/views/web/pages/home.blade.php
+++ resources/views/web/pages/home.blade.php
@@ -11,12 +11,13 @@
                     <h6 class="m-0">Categories</h6>
                     <i class="fa fa-angle-down text-dark"></i>
                 </a>
-                <nav class="collapse show navbar navbar-vertical navbar-light align-items-start p-0 border border-top-0 border-bottom-0"
-                    id="navbar-vertical">
-                    <div class="navbar-nav w-100 overflow-hidden" style="height: 410px">
+                <nav class="collapse position-absolute show navbar navbar-vertical navbar-light align-items-start p-0 border border-top-0 border-bottom-0 bg-light"
+                    id="navbar-vertical" style="width: calc(100% - 30px); z-index: 1;">
+                    <div class="navbar-nav w-100">
                         @foreach (App\Models\Category::where('active', 1)->get()->sortBy('position')->take(10) as $categories_search)
-                            <a href="{{ route('products.category', $categories_search->id) }}"
-                                class="nav-item nav-link">{{ $categories_search->title ?? '' }}</a>
+                            <a href="{{ route('products.category', $categories_search->id) }}" class="nav-item nav-link">
+                                {{ $categories_search->title ?? '' }}
+                            </a>
                         @endforeach
                     </div>
                 </nav>
