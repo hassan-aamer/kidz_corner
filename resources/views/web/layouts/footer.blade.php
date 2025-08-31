0dfd0b3c45083f05d79b5b5a675278e1aca2c66c
hassan-aamer <hassanaamer048@gmail.com>
2025-08-31 23:34:16 +0300

    u

--- resources/views/web/layouts/footer.blade.php
+++ resources/views/web/layouts/footer.blade.php
@@ -1,46 +1,51 @@
     <!-- Footer Start -->
-<div class="container-fluid mt-5 pt-5"
-     style="background: url('{{ asset('public/web/img/fotter.jpg') }}') no-repeat center center/cover;">
-    <div class="row px-xl-5 pt-5 text-white">
-        <div class="col-lg-8 col-md-12 mb-5 pr-3 pr-xl-5">
-            <a href="{{ route('home') }}" class="text-decoration-none">
-                <img class="mb-4 display-5 font-weight-semi-bold"
-                     src="{{ App\Helpers\Image::getMediaUrl(App\Models\Setting::first(), 'logo') }}"
-                     alt="{{ setting('name') ?? '' }}" height="70" width="170">
-            </a>
-            @if (setting('address'))
-                <p class="mb-2"><i class="fa fa-map-marker-alt text-primary mr-3"></i>{{ setting('address') ?? '' }}</p>
-            @endif
-            @if (setting('email'))
-                <p class="mb-2"><i class="fa fa-envelope text-primary mr-3"></i>{{ setting('email') ?? '' }}</p>
-            @endif
-            @if (setting('phone'))
-                <p class="mb-0"><i class="fa fa-phone-alt text-primary mr-3"></i>{{ setting('phone') ?? '' }}</p>
-            @endif
-        </div>
-        <div class="col-lg-4 col-md-12">
-            <div class="row">
-                <div class="col-md-6 mb-5">
-                    <h5 class="font-weight-bold text-white mb-4">Quick Links</h5>
-                    <div class="d-flex flex-column justify-content-start">
-                        <a class="text-white mb-2" href="{{ route('home') }}"><i class="fa fa-angle-right mr-2"></i>Home</a>
-                        <a class="text-white mb-2" href="{{ route('products') }}"><i class="fa fa-angle-right mr-2"></i>Shop</a>
-                        <a class="text-white" href="{{ route('contact') }}"><i class="fa fa-angle-right mr-2"></i>Contact</a>
+    <div class="container-fluid mt-5 pt-5"
+        style="background: url('{{ asset('public/web/img/fotter.jpg') }}') no-repeat center center/cover;">
+        <div class="row px-xl-5 pt-5 text-white">
+            <div class="col-lg-8 col-md-12 mb-5 pr-3 pr-xl-5">
+                <a href="{{ route('home') }}" class="text-decoration-none">
+                    <img class="mb-4 display-5 font-weight-semi-bold"
+                        src="{{ App\Helpers\Image::getMediaUrl(App\Models\Setting::first(), 'logo') }}"
+                        alt="{{ setting('name') ?? '' }}" height="70" width="170">
+                </a>
+                @if (setting('address'))
+                    <p class="mb-2"><i
+                            class="fa fa-map-marker-alt text-primary mr-3"></i>{{ setting('address') ?? '' }}</p>
+                @endif
+                @if (setting('email'))
+                    <p class="mb-2"><i class="fa fa-envelope text-primary mr-3"></i>{{ setting('email') ?? '' }}</p>
+                @endif
+                @if (setting('phone'))
+                    <p class="mb-0"><i class="fa fa-phone-alt text-primary mr-3"></i>{{ setting('phone') ?? '' }}</p>
+                @endif
+            </div>
+            <div class="col-lg-4 col-md-12">
+                <div class="row">
+                    <div class="col-md-6 mb-5">
+                        <h5 class="font-weight-bold text-white mb-4">Quick Links</h5>
+                        <div class="d-flex flex-column justify-content-start">
+                            <a class="text-white mb-2" href="{{ route('home') }}"><i
+                                    class="fa fa-angle-right mr-2"></i>Home</a>
+                            <a class="text-white mb-2" href="{{ route('products') }}"><i
+                                    class="fa fa-angle-right mr-2"></i>Shop</a>
+                            <a class="text-white" href="{{ route('contact') }}"><i
+                                    class="fa fa-angle-right mr-2"></i>Contact</a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="row mx-xl-5 py-4 text-white">
-        <div class="col-md-6 px-xl-0">
-            <p class="mb-md-0 text-center text-md-left">
-                <a class="text-white font-weight-semi-bold" href="{{ route('home') }}">{{ setting('copyrights') ?? '' }}</a>. Powered
-                by
-                <a class="text-white font-weight-semi-bold" href="https://viral-monkeys.com">Viral Monkeys</a><br>
-            </p>
+        <div class="row mx-xl-5 py-4 text-white">
+            <div class="col-md-6 px-xl-0">
+                <p class="mb-md-0 text-center text-md-left">
+                    <a class="text-white font-weight-semi-bold"
+                        href="{{ route('home') }}">{{ setting('copyrights') ?? '' }}</a>. Powered
+                    by
+                    <a class="text-white font-weight-semi-bold" href="https://viral-monkeys.com">Viral Monkeys</a><br>
+                </p>
+            </div>
         </div>
     </div>
-</div>
 
     <!-- Footer End -->
 
--- resources/views/web/pages/home.blade.php
+++ resources/views/web/pages/home.blade.php
@@ -145,13 +145,14 @@
 
 
     <!-- Subscribe Start -->
-    <div class="container-fluid bg-secondary my-5">
+    <div class="container-fluid bg-secondary my-5" style="background: url('{{ asset('public/web/img/Stay Updated section.jpg') }}') no-repeat center center/cover;">
         <div class="row justify-content-md-center py-5 px-xl-5">
             <div class="col-md-6 col-12 py-5">
                 <div class="text-center mb-2 pb-2">
-                    <h2 class="section-title px-5 mb-3"><span class="bg-secondary px-2">Stay Updated</span></h2>
-                    <p>Amet lorem at rebum amet dolores. Elitr lorem dolor sed amet diam labore at justo ipsum eirmod duo
-                        labore labore.</p>
+                    {{-- <h2 class="section-title px-5 mb-3"><span class=" px-2">Stay Updated</span></h2> --}}
+                    <h2 style="color: whitesmoke;">Stay Updated</h2>
+                    {{-- <p>Amet lorem at rebum amet dolores. Elitr lorem dolor sed amet diam labore at justo ipsum eirmod duo
+                        labore labore.</p> --}}
                 </div>
                 <form action="{{ route('subscription') }}" method="POST">
                     @csrf
