af1bea40727443363c70b00bae6356b378216d04
hassan-aamer <hassanaamer048@gmail.com>
2025-08-31 10:30:29 +0300

    update

--- resources/views/web/layouts/app.blade.php
+++ resources/views/web/layouts/app.blade.php
@@ -6,10 +6,6 @@
 </head>
 
 <body>
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5P278XRM" height="0" width="0"
-            style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
     @include('web.layouts.header')
     @yield('content')
     @include('web.layouts.footer')
