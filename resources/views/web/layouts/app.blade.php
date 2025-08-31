cbfcb512177d88f4856d67e529de308a38826c48
hassan-aamer <hassanaamer048@gmail.com>
2025-08-31 10:21:53 +0300

    update

--- resources/views/web/layouts/app.blade.php
+++ resources/views/web/layouts/app.blade.php
@@ -2,10 +2,14 @@
 <html lang="en">
 
 <head>
-@include('web.layouts.head')
+    @include('web.layouts.head')
 </head>
 
 <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5P278XRM" height="0" width="0"
+            style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     @include('web.layouts.header')
     @yield('content')
     @include('web.layouts.footer')
