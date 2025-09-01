c31b4dbdaf29aa02335b1a529647233b63bef629
hassan-aamer <hassanaamer048@gmail.com>
2025-09-01 11:56:06 +0300

    update

--- resources/views/web/layouts/js.blade.php
+++ resources/views/web/layouts/js.blade.php
@@ -1,8 +1,10 @@
     <!-- JavaScript Libraries -->
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
-    <script src="{{ asset('web/lib/easing/easing.min.js') }}"></script>
-    <script src="{{ asset('web/lib/owlcarousel/owl.carousel.min.js') }}"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" async></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js" async></script>
+    <script src="{{ asset('web/lib/easing/easing.min.js') }}" async></script>
+    <script src="{{ asset('web/lib/owlcarousel/owl.carousel.min.js') }}" async></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js" async></script>
+
 
     <!-- Contact Javascript File -->
     <script src="{{ asset('web/mail/jqBootstrapValidation.min.js') }}"></script>
