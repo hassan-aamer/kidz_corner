50c675bc90d7d236dec957af908dd3afb6352fa5
hassan-aamer <hassanaamer048@gmail.com>
2025-09-01 12:22:41 +0300

    u

--- resources/views/web/pages/product.blade.php
+++ resources/views/web/pages/product.blade.php
@@ -117,9 +117,9 @@
     @if ($result['relatedProducts']->count())
         <!-- Products Start -->
         <div class="container-fluid py-5">
-            <div class="text-center mb-4">
+            {{-- <div class="text-center mb-4">
                 <h2 class="section-title px-5"><span class="px-2">You May Also Like</span></h2>
-            </div>
+            </div> --}}
             <div class="row px-xl-5">
                 <div class="col">
                     <div class="owl-carousel related-carousel">
