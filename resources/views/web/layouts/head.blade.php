b2900f6ebd0b0304635e8e68b3fb80ecaf2050e7
hassan-aamer <hassanaamer048@gmail.com>
2025-10-30 17:22:14 +0300

    update

--- resources/views/web/layouts/head.blade.php
+++ resources/views/web/layouts/head.blade.php
@@ -4,7 +4,7 @@
 <meta content="Free HTML Templates" name="keywords">
 <meta content="Free HTML Templates" name="description">
 <link rel="canonical" href="{{ url()->current() }}">
-<script type="application/ld+json">
+{{-- <script type="application/ld+json">
         {
         "@context": "https://schema.org",
         "@type": "Organization",
@@ -24,22 +24,10 @@
             "{{ setting('twitter') ?? '' }}"
         ]
         }
-    </script>
+    </script> --}}
 
 @include('web.layouts.css')
 @yield('css')
-
-<!-- Google Tag Manager -->
-{{-- <script>(function (w, d, s, l, i) {
-        w[l] = w[l] || []; w[l].push({
-            'gtm.start':
-                new Date().getTime(), event: 'gtm.js'
-        }); var f = d.getElementsByTagName(s)[0],
-            j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
-                'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-    })(window, document, 'script', 'dataLayer', 'GTM-MSR7PB6M');</script> --}}
-<!-- End Google Tag Manager -->
-
 
 
 
@@ -56,3 +44,31 @@
 })(window,document,'script','dataLayer','GTM-MSR7PB6M');
 </script>
 <!-- End Google Tag Manager -->
+
+
+    <!-- Facebook Pixel Base Code -->
+    <script>
+        !function(f,b,e,v,n,t,s){
+            if(f.fbq)return;n=f.fbq=function(){
+                n.callMethod? n.callMethod.apply(n,arguments):n.queue.push(arguments)
+            };
+            if(!f._fbq)n=n.fbq=n;
+            n.push=n;
+            n.loaded=!0;
+            n.version='2.0';
+            n.queue=[];
+            t=b.createElement(e);t.async=!0;
+            t.src=v;
+            s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)
+        }(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+        fbq('init', '4251841431763780');
+        fbq('track', 'PageView');
+    </script>
+
+    <noscript>
+        <img height="1" width="1" style="display:none"
+             src="https://www.facebook.com/tr?id=4251841431763780&ev=PageView&noscript=1"/>
+    </noscript>
+    <!-- End Facebook Pixel Code -->
