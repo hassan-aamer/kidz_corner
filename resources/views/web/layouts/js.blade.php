22851e52ad2050f36604c8856f119f319d8175c7
hassan-aamer <hassanaamer048@gmail.com>
2025-11-04 03:56:37 +0200

    u

--- resources/views/web/layouts/js.blade.php
+++ resources/views/web/layouts/js.blade.php
@@ -1,21 +1,24 @@
-    <!-- JavaScript Libraries -->
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js" async></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js" async></script>
-    <script src="{{ asset('web/lib/easing/easing.min.js') }}" async></script>
-    <script src="{{ asset('web/lib/owlcarousel/owl.carousel.min.js') }}" async></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js" async></script>
-    {{-- <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js" async></script> --}}
+<!-- ✅ أولاً: jQuery -->
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 
+<!-- ✅ ثانياً: Bootstrap -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
 
+<!-- ✅ ثالثاً: مكتبات أخرى تعتمد على jQuery -->
+<script src="{{ asset('web/lib/easing/easing.min.js') }}"></script>
+<script src="{{ asset('web/lib/owlcarousel/owl.carousel.min.js') }}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js"></script>
 
-    <!-- Contact Javascript File -->
-    <script src="{{ asset('web/mail/jqBootstrapValidation.min.js') }}"></script>
-    <script src="{{ asset('web/mail/contact.js') }}"></script>
+<!-- ✅ Contact & Validation -->
+<script src="{{ asset('web/mail/jqBootstrapValidation.min.js') }}"></script>
+<script src="{{ asset('web/mail/contact.js') }}"></script>
 
-    <!-- Template Javascript -->
-    <script src="{{ asset('web/js/main.js') }}"></script>
-    @include('admin.layouts.sweetalert')
-    @yield('js')
+<!-- ✅ سكربتات المشروع -->
+<script src="{{ asset('web/js/main.js') }}"></script>
+
+@include('admin.layouts.sweetalert')
+@yield('js')
+
 
     {{-- <script>
         document.addEventListener('contextmenu', function(e) {
