d45536876159b226a071fa1559ee74fc22f7b149
hassan-aamer <hassanaamer048@gmail.com>
2025-11-04 10:07:46 +0200

    u

--- resources/views/web/components/category-dropdown.blade.php
+++ resources/views/web/components/category-dropdown.blade.php
@@ -1,5 +1,5 @@
 <nav class="collapse position-absolute navbar navbar-vertical navbar-light align-items-start p-0 border border-top-0 border-bottom-0 bg-light"
-    id="navbar-vertical" style="width: calc(100% - 30px); z-index: 1;">
+    id="navbar-vertical" style="width: calc(100% - 30px); z-index: 1;box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);">
     <div class="navbar-nav w-100">
         @foreach (App\Models\Category::where('active', 1)->get()->sortBy('position')->take(10) as $categories_search)
             <a href="{{ route('products.category', $categories_search->id) }}" class="nav-item nav-link">
