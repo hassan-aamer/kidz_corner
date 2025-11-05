cfca75584be99056c8d1320f7ce86645dacf3677
hassan-aamer <hassanaamer048@gmail.com>
2025-11-05 15:55:16 +0200

    u

--- resources/views/web/layouts/head.blade.php
+++ resources/views/web/layouts/head.blade.php
@@ -30,50 +30,6 @@
 @include('web.layouts.css')
 @yield('css')
 
-
-
-
-<!-- Google Tag Manager -->
-{{-- <script>
-(function(w,d,s,l,i){
-  w[l]=w[l]||[];
-  w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-  var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
-  j.async=true; j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-  f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MSR7PB6M');
-</script> --}}
-<!-- End Google Tag Manager -->
-
-
-    <!-- Facebook Pixel Base Code -->
-    {{-- <script>
-        !function(f,b,e,v,n,t,s){
-            if(f.fbq)return;n=f.fbq=function(){
-                n.callMethod? n.callMethod.apply(n,arguments):n.queue.push(arguments)
-            };
-            if(!f._fbq)n=n.fbq=n;
-            n.push=n;
-            n.loaded=!0;
-            n.version='2.0';
-            n.queue=[];
-            t=b.createElement(e);t.async=!0;
-            t.src=v;
-            s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)
-        }(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
-
-        fbq('init', '4251841431763780');
-        fbq('track', 'PageView');
-    </script>
-
-    <noscript>
-        <img height="1" width="1" style="display:none"
-             src="https://www.facebook.com/tr?id=4251841431763780&ev=PageView&noscript=1"/>
-    </noscript> --}}
-    <!-- End Facebook Pixel Code -->
-
 <!-- Meta Pixel Code -->
 <script>
 !function(f,b,e,v,n,t,s)
@@ -91,3 +47,12 @@ fbq('track', 'PageView');
 src="https://www.facebook.com/tr?id=1380345543791870&ev=PageView&noscript=1"
 /></noscript>
 <!-- End Meta Pixel Code -->
+
+
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "u1dnbfdgc3");
+</script>
