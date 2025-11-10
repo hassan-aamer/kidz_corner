c6093d56a838160b53319267c387a90569ad6b85
hassan-aamer <hassanaamer048@gmail.com>
2025-11-10 17:39:30 +0200

    u

--- resources/views/web/layouts/app.blade.php
+++ resources/views/web/layouts/app.blade.php
@@ -3,13 +3,22 @@
 
 <head>
     @include('web.layouts.head')
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-MF4ZG89Y2G"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+
+        gtag('config', 'G-MF4ZG89Y2G');
+    </script>
 </head>
 
 <body style="font-family: 'Cairo', sans-serif">
     <!-- Google Tag Manager (noscript) -->
     <noscript>
-        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MNPQV6BV"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MNPQV6BV" height="0" width="0"
+            style="display:none;visibility:hidden"></iframe>
     </noscript>
     <!-- End Google Tag Manager (noscript) -->
     @include('web.layouts.header')
