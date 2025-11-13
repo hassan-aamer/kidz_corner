d91de523be40c1e99c8c88086c59cf481bb362f0
hassan-aamer <hassanaamer048@gmail.com>
2025-11-13 21:34:40 +0200

    update

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Web;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Support\Facades\Cache;
 use App\Services\Reviews\ReviewService;
 use App\Services\Banners\BannersService;
@@ -21,18 +22,28 @@ class HomeController extends Controller
     public function __construct(
         BannersService $bannersService,
         CategoryService $categoryService
-        )
-    {
+    ) {
         $this->categoryService = $categoryService;
         $this->bannersService   = $bannersService;
     }
     public function index(Request $request)
     {
         $result = [
-            'banners'           => $this->bannersService->index($request),
-            'categories'        => Cache::remember('home_categories', now()->addHours(3), function () {
-            return $this->categoryService->index()->where('active', 1)->take(6);
-        }),
+            'banners' => Cache::remember('home_banners', now()->addHours(6), function () use ($request) {
+                return $this->bannersService->index($request);
+            }),
+            'categories' => Cache::remember('home_categories', now()->addHours(6), function () {
+                return Category::with(['products' => function ($query) {
+                    $query->where('active', 1)
+                        ->orderBy('position', 'asc')  // ترتيب حسب الحقل position
+                        ->take(6);                   // أول 6 منتجات فقط
+                }])
+                    ->where('active', 1)
+                    ->take(6)                             // أول 6 فئات
+                    ->get();
+            }),
+
+
 
         ];
         return view('web.pages.home', compact('result'));
