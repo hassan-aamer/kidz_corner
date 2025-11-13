a9b3f37d05142234c235ea19ca18a5e5a1d34f90
hassan-aamer <hassanaamer048@gmail.com>
2025-11-14 00:54:45 +0200

    u

--- app/Http/Controllers/MetaCapiController.php
+++ app/Http/Controllers/MetaCapiController.php
@@ -7,32 +7,79 @@ use Illuminate\Support\Facades\Http;
 
 class MetaCapiController extends Controller
 {
+    // public function handle(Request $request)
+    // {
+    //     $data = $request->all();
+
+    //     $access_token = env('META_ACCESS_TOKEN');
+    //     $pixel_id = env('META_PIXEL_ID');
+
+    //     $response = Http::post("https://graph.facebook.com/v18.0/{$pixel_id}/events", [
+    //         'data' => [[
+    //             'event_name' => 'Purchase',
+    //             'event_time' => time(),
+    //             'event_id' => $data['event_id'],
+    //             'action_source' => 'website',
+    //             'user_data' => [
+    //                 'em' => hash('sha256', $data['customer']['email']),
+    //                 'ph' => hash('sha256', $data['customer']['phone']),
+    //             ],
+    //             'custom_data' => [
+    //                 'value' => $data['value'],
+    //                 'currency' => $data['currency'],
+    //                 'order_id' => $data['order_id'],
+    //             ]
+    //         ]],
+    //         'access_token' => $access_token
+    //     ]);
+
+    //     return response()->json($response->json());
+    // }
+
     public function handle(Request $request)
     {
         $data = $request->all();
 
-        $access_token = env('META_ACCESS_TOKEN');
         $pixel_id = env('META_PIXEL_ID');
+        $access_token = env('META_ACCESS_TOKEN');
 
-        $response = Http::post("https://graph.facebook.com/v18.0/{$pixel_id}/events", [
-            'data' => [[
-                'event_name' => 'Purchase',
-                'event_time' => time(),
-                'event_id' => $data['event_id'],
-                'action_source' => 'website',
-                'user_data' => [
-                    'em' => hash('sha256', $data['customer']['email']),
-                    'ph' => hash('sha256', $data['customer']['phone']),
+        // Normalize & Hash data
+        $email = strtolower(trim($data['customer']['email'] ?? ''));
+        $phone = preg_replace('/[^0-9]/', '', $data['customer']['phone'] ?? '');
+
+        $hashedEmail = $email ? hash('sha256', $email) : null;
+        $hashedPhone = $phone ? hash('sha256', $phone) : null;
+
+        $eventBody = [
+            "data" => [[
+                "event_name" => "Purchase",
+                "event_time" => time(),
+                "event_id" => $data["event_id"],  // deduplication ID
+                "action_source" => "website",
+
+                "user_data" => [
+                    "em" => $hashedEmail ? [$hashedEmail] : [],
+                    "ph" => $hashedPhone ? [$hashedPhone] : [],
+                    "client_ip_address" => $request->ip(),
+                    "client_user_agent" => $_SERVER['HTTP_USER_AGENT'] ?? '',
                 ],
-                'custom_data' => [
-                    'value' => $data['value'],
-                    'currency' => $data['currency'],
-                    'order_id' => $data['order_id'],
-                ]
-            ]],
-            'access_token' => $access_token
-        ]);
 
-        return response()->json($response->json());
+                "custom_data" => [
+                    "value" => $data["value"],
+                    "currency" => $data["currency"],
+                    "order_id" => $data["order_id"],
+                ],
+            ]]
+        ];
+
+        // Send request correctly (token in URL)
+        $url = "https://graph.facebook.com/v18.0/{$pixel_id}/events?access_token={$access_token}";
+
+        $response = Http::post($url, $eventBody);
+
+        return response()->json([
+            "sent" => $eventBody,
+            "fb_response" => $response->json()
+        ]);
     }
 }
