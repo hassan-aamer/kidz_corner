15886cb7ceed5148571d2399ef5629ef88f8ad4e
hassan-aamer <hassanaamer048@gmail.com>
2025-11-14 16:25:33 +0200

    update

--- resources/views/web/layouts/head.blade.php
+++ resources/views/web/layouts/head.blade.php
@@ -1,8 +1,8 @@
 <meta charset="utf-8">
 <title>@yield('title') | {{ setting('name') }}</title>
 <meta content="width=device-width, initial-scale=1.0" name="viewport">
-<meta content="Free HTML Templates" name="keywords">
-<meta content="Free HTML Templates" name="description">
+<meta content="{{ setting('name') }}" name="keywords">
+<meta content="{{ setting('name') }}" name="description">
 <link rel="canonical" href="{{ url()->current() }}">
 
 
