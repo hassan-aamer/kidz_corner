baeb07b6484339e8333ba20a3928030ee525595e
hassan-aamer <hassanaamer048@gmail.com>
2025-11-14 16:51:41 +0200

    update

--- resources/views/web/layouts/head.blade.php
+++ resources/views/web/layouts/head.blade.php
@@ -1,9 +1,10 @@
 <meta charset="utf-8">
 <title>@yield('title') | {{ setting('name') }}</title>
 <meta content="width=device-width, initial-scale=1.0" name="viewport">
-<meta content="{{ setting('description') }}" name="keywords">
-<meta content="{{ setting('description') }}" name="description">
-{{-- <meta property="og:url" content="https://kidzcorner.shop"> --}}
+<meta property="og:title" content="{{ setting('name') }}">
+<meta property="og:description" content="{{ setting('description') }}">
+{{-- <meta content="{{ setting('description') }}" name="keywords">
+<meta content="{{ setting('description') }}" name="description"> --}}
 <link rel="canonical" href="{{ url()->current() }}">
 
 
@@ -12,29 +13,34 @@
 
 <!-- Meta Pixel Code -->
 <script>
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1099641122366792');
-fbq('track', 'PageView');
+    !function (f, b, e, v, n, t, s) {
+        if (f.fbq) return; n = f.fbq = function () {
+            n.callMethod ?
+            n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+        };
+        if (!f._fbq) f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0';
+        n.queue = []; t = b.createElement(e); t.async = !0;
+        t.src = v; s = b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t, s)
+    }(window, document, 'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '1099641122366792');
+    fbq('track', 'PageView');
 </script>
 <noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=1099641122366792&ev=PageView&noscript=1"
-/></noscript>
+        src="https://www.facebook.com/tr?id=1099641122366792&ev=PageView&noscript=1" /></noscript>
 <!-- End Meta Pixel Code -->
 
 
 
 
 <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WKTKZ8H2');</script>
+<script>(function (w, d, s, l, i) {
+        w[l] = w[l] || []; w[l].push({
+            'gtm.start':
+                new Date().getTime(), event: 'gtm.js'
+        }); var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-WKTKZ8H2');</script>
 <!-- End Google Tag Manager -->
